283c70f161813f7b47e308b0860e8dd8fc6afa0d
amohdi <mohdiabdo47@gmail.com>
2025-06-02 00:50:54 +0100

    solved the sqrt problem, still to reimplement it using the binary search algorithm

---
+++ Medium_problems/Sqrt.cpp
@@ -0,0 +1,22 @@
+// class Solution {
+// public:
+//     int mySqrt(int x) {
+//         int result = 1;
+//         while(result <= (x / result)) {
+//             if ( (result * result) == x )
+//                 return result;
+//             ++result;
+//         }
+//         return result - 1;
+//     }
+// };
+
+// using binary seach to find the square root
+
+
+class Solution {
+public:
+   int mySqrt(int x) {
+       
+   }
+};
